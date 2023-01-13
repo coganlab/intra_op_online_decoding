4a3fd397667cc2adb366d0985e51942755e0be12
Zac Spalding <spaldingzac@gmail.com>
2023-01-13 15:03:41 -0500

    outline file for simulation of data streaming

---
+++ python_implementation/data_streaming/simulate_streaming_rhd.py
@@ -0,0 +1,17 @@
+"""
+FILE INTRODUCTION TODO
+
+Author: Zac Spalding
+"""
+
+
+def load_intan_rhd_format(recording_dir, verbose=True):
+    pass
+
+
+def calculate_num_chunks(in_data):
+    pass
+
+
+def create_data_queue(in_data, num_chunks):
+    pass
