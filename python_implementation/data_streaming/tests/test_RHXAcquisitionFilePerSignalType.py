8def4b032aaa2506036fe58368349f7afecde16b
Zac Spalding <spaldingzac@gmail.com>
2023-01-13 15:03:30 -0500

    fixed import structure in acquisition test file

--- python_implementation/data_streaming/tests/test_RHXAcquisitionFilePerSignalType.py
+++ python_implementation/data_streaming/tests/test_RHXAcquisitionFilePerSignalType.py
@@ -1,20 +1,7 @@
 import pytest
-# add parent dir to path for imports
-import sys
 import os
 
-# getting the name of the directory
-# where the this file is present.
 current = os.path.dirname(os.path.realpath(__file__))
-
-# Getting the parent directory name
-# where the current directory is present.
-parent = os.path.dirname(current)
-
-# adding the parent directory to
-# the sys.path.
-sys.path.append(parent)
-
 test_data_dir = current + "/../../../test_data"
 
 
